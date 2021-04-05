bba34e178036e276190cd2df8af14c256bcd26d0
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2021-04-05 10:51:11 -0400

    Removed html

--- core/src/changeme/snake/GameLogic.java
+++ core/src/changeme/snake/GameLogic.java
@@ -138,11 +138,21 @@ public class GameLogic {
     }
 
     public void move() {
-        Coord toSquare = switch (direction) {
-            case NORTH -> new Coord(head.x(), head.y() + 1);
-            case SOUTH -> new Coord(head.x(), head.y() - 1);
-            case EAST -> new Coord(head.x() + 1, head.y());
-            case WEST -> new Coord(head.x() - 1, head.y());
+//        Coord toSquare = switch (direction) {
+//            case NORTH -> new Coord(head.x(), head.y() + 1);
+//            case SOUTH -> new Coord(head.x(), head.y() - 1);
+//            case EAST -> new Coord(head.x() + 1, head.y());
+//            case WEST -> new Coord(head.x() - 1, head.y());
+//        };
+
+        Coord toSquare;
+        switch (direction) {
+            case NORTH: toSquare = new Coord(head.x(), head.y() + 1); break;
+            case SOUTH: toSquare = new Coord(head.x(), head.y() - 1); break;
+            case EAST: toSquare = new Coord(head.x() + 1, head.y()); break;
+            case WEST: toSquare =  new Coord(head.x() - 1, head.y()); break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + direction);
         };
 
         Coord tailTo = tailBody.size() > 0 ? tailBody.get(tailBody.size() - 1).copy() : head.copy();
